971d625bb50be0aa30debdd9930bf598c4c27233
Simon Long <simon@raspberrypi.com>
2023-05-09 14:54:00 +0100

    Remove need to run under sudo

--- src/main.c
+++ src/main.c
@@ -74,9 +74,7 @@ int main (int argc, char *argv[])
     gtk_init (&argc, &argv);
 
     page_size=sysconf(_SC_PAGESIZE)>>10;
-    char *uid = getenv ("SUDO_UID");
-    if (!uid) own_uid = getuid();
-    else sscanf (uid, "%d", &own_uid);
+    own_uid = getuid();
 
     config_file = g_build_filename(g_get_user_config_dir(), "lxtask.conf", NULL);
     load_config();
